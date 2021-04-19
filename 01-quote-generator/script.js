a6e2adbc21206d6f1e70a5093a3655aaa25592c3
Andrii Stepanets <ovrass@gmail.com>
2021-04-19 22:20:29 +0300

    added condition for unknown author and calculate length quote

--- 01-quote-generator/script.js
+++ 01-quote-generator/script.js
@@ -9,13 +9,29 @@
 // }
 // newRandomQuotes()
 // Using Fetch API
+
+const quoteContainer = document.querySelector('#quote-container')
+const quoteText = document.querySelector('#quote')
+const authorText = document.querySelector('#author')
+const twitterBtn = document.querySelector('#twitter')
+const newQuoteBtn = document.querySelector('#new-quote')
+
 let apiQuotes = []
 
 
 function newRandomQuotes() {
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  console.log(quote);
+  // check if Author field is blank and replace it 
+  !quote.author
+    ? (authorText.textContent = 'Author Unknown')
+    : (authorText.textContent = quote.author);
+
+  //Check Quote length to determine styling 
+  (quoteText.textContent.length > 120)
+    ? quoteText.classList.add('long-quote')
+    : quoteText.classList.remove('long-quote')
+  quoteText.textContent = quote.text;
 }
 
 // Get Quotes From API 
