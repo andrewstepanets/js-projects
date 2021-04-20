f85d567f425b93a13c90bf682f74f877012f174b
Andrii Stepanets <ovrass@gmail.com>
2021-04-20 20:11:44 +0300

    Completed fetch quotes project

--- 01-quote-generator/script.js
+++ 01-quote-generator/script.js
@@ -15,11 +15,26 @@ const quoteText = document.querySelector('#quote')
 const authorText = document.querySelector('#author')
 const twitterBtn = document.querySelector('#twitter')
 const newQuoteBtn = document.querySelector('#new-quote')
+const loader = document.querySelector('#loader')
 
 let apiQuotes = []
 
+// Show loading
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loading 
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
 
 function newRandomQuotes() {
+  loading()
   // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   // check if Author field is blank and replace it 
@@ -31,12 +46,16 @@ function newRandomQuotes() {
   (quoteText.textContent.length > 120)
     ? quoteText.classList.add('long-quote')
     : quoteText.classList.remove('long-quote')
+
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API 
 
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl)
@@ -47,5 +66,17 @@ async function getQuotes() {
     console.log(error);
   }
 }
+
+
+// Tweet Quote 
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`
+  window.open(twitterUrl, '_blank')
+}
+
+// Event Listeners 
+newQuoteBtn.addEventListener('click', newRandomQuotes)
+twitterBtn.addEventListener('click', tweetQuote)
 
 getQuotes()
